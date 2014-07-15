e6ad0fe814eb4024887175035ba0c9b34aaca830
somecallmetim <timbauer@ymail.com>
2014-07-15 12:40:30 -0700

    added first bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new teacherApp\HomeBundle\HomeBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
