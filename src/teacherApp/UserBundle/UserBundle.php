9c0e9602a6792d270fb1a6131587173431240013
somecallmetim <timbauer@ymail.com>
2014-07-17 18:45:09 -0700

    got FosUserBundle installed and running. Still has default pages

---
+++ src/teacherApp/UserBundle/UserBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: timbauer
+ * Date: 7/17/14
+ * Time: 6:15 PM
+ */
+
+namespace teacherApp\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UserBundle extends Bundle {
+
+} 
